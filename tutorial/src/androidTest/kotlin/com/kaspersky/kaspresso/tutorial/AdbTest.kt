2169a104df4fb1ebe858631fdef850a9183fb93e
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-30 07:18:10 +0300

    Добавил несколько тестов в AdbTest.kt

--- tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/AdbTest.kt
+++ tutorial/src/androidTest/kotlin/com/kaspersky/kaspresso/tutorial/AdbTest.kt
@@ -7,10 +7,28 @@ import org.junit.Test
 class AdbTest : TestCase() {
 
     @Test
-    fun testAdb() {
+    fun adbDevices() {
         val res = adbServer.performAdb("devices")
         Assert.assertTrue(
             res.first().contains("emulator")
+        )
+    }
+
+    @Test
+    fun adbListPackages() {
+        /*val res = adbServer.performShell("pm list packages")
+        Assert.assertTrue(
+            device.targetContext.packageName in res.first()
+        )*/
+
+        val packages = adbServer.performShell("pm list packages")
+        Assert.assertTrue(device.targetContext.packageName in packages.first())
+    }
+
+    @Test
+    fun adbAmd() {
+        Assert.assertTrue(
+            "Andrey" in adbServer.performCmd("echo %USERNAME%")
         )
     }
 }
